973f17d69722c21affc395a10187779d6123a3c3
buetz <alexander.m.richter@gmail.com>
2014-01-09 22:55:10 +0100

    filter out already known(lookedup) items

--- Dota2BuyMonitor/Form1.Designer.cs
+++ Dota2BuyMonitor/Form1.Designer.cs
@@ -39,6 +39,8 @@
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
             this.buttonStart = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
             this.groupBoxOptions.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.groupBoxItems.SuspendLayout();
@@ -51,11 +53,13 @@
             // 
             // groupBoxOptions
             // 
+            this.groupBoxOptions.Controls.Add(this.checkBox2);
+            this.groupBoxOptions.Controls.Add(this.checkBox1);
             this.groupBoxOptions.Controls.Add(this.label1);
             this.groupBoxOptions.Controls.Add(this.numericUpDown1);
             this.groupBoxOptions.Location = new System.Drawing.Point(12, 12);
             this.groupBoxOptions.Name = "groupBoxOptions";
-            this.groupBoxOptions.Size = new System.Drawing.Size(286, 51);
+            this.groupBoxOptions.Size = new System.Drawing.Size(286, 94);
             this.groupBoxOptions.TabIndex = 0;
             this.groupBoxOptions.TabStop = false;
             this.groupBoxOptions.Text = "Options";
@@ -95,9 +99,9 @@
             // groupBoxItems
             // 
             this.groupBoxItems.Controls.Add(this.richTextBox1);
-            this.groupBoxItems.Location = new System.Drawing.Point(12, 107);
+            this.groupBoxItems.Location = new System.Drawing.Point(12, 141);
             this.groupBoxItems.Name = "groupBoxItems";
-            this.groupBoxItems.Size = new System.Drawing.Size(286, 222);
+            this.groupBoxItems.Size = new System.Drawing.Size(286, 235);
             this.groupBoxItems.TabIndex = 1;
             this.groupBoxItems.TabStop = false;
             this.groupBoxItems.Text = "Items";
@@ -105,10 +109,10 @@
             // richTextBox1
             // 
             this.richTextBox1.BackColor = System.Drawing.SystemColors.Control;
-            this.richTextBox1.Location = new System.Drawing.Point(7, 20);
+            this.richTextBox1.Location = new System.Drawing.Point(7, 19);
             this.richTextBox1.Name = "richTextBox1";
             this.richTextBox1.ReadOnly = true;
-            this.richTextBox1.Size = new System.Drawing.Size(273, 186);
+            this.richTextBox1.Size = new System.Drawing.Size(273, 210);
             this.richTextBox1.TabIndex = 0;
             this.richTextBox1.Text = "";
             this.richTextBox1.WordWrap = false;
@@ -124,7 +128,7 @@
             // 
             // buttonStart
             // 
-            this.buttonStart.Location = new System.Drawing.Point(12, 78);
+            this.buttonStart.Location = new System.Drawing.Point(12, 112);
             this.buttonStart.Name = "buttonStart";
             this.buttonStart.Size = new System.Drawing.Size(75, 23);
             this.buttonStart.TabIndex = 2;
@@ -132,11 +136,31 @@
             this.buttonStart.UseVisualStyleBackColor = true;
             this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
             // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(150, 21);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(80, 17);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(9, 49);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(80, 17);
+            this.checkBox2.TabIndex = 3;
+            this.checkBox2.Text = "checkBox2";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(310, 341);
+            this.ClientSize = new System.Drawing.Size(310, 388);
             this.Controls.Add(this.buttonStart);
             this.Controls.Add(this.groupBoxItems);
             this.Controls.Add(this.groupBoxOptions);
@@ -163,6 +187,8 @@
         private System.Windows.Forms.Timer timer1;
         private System.ComponentModel.BackgroundWorker backgroundWorker1;
         private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
     }
 }
 
--- Dota2BuyMonitor/Form1.cs
+++ Dota2BuyMonitor/Form1.cs
@@ -17,8 +17,11 @@ namespace Dota2BuyMonitor
         public Form1()
         {
             InitializeComponent();
-        }
 
+            //add eventlisteners for backgroundworkers at the very beginning
+            backgroundWorker1.RunWorkerCompleted += new RunWorkerCompletedEventHandler(backgroundWorker1_RunWorkerCompleted);
+        }
+        
         private void buttonStart_Click(object sender, EventArgs e)
         {
             groupBoxOptions.Enabled = false;
@@ -27,6 +30,7 @@ namespace Dota2BuyMonitor
             timer1.Enabled = true;
             timer1.Interval = Convert.ToInt16(numericUpDown1.Value);
             timer1.Start();
+
         }
 
         private void timer1_Tick(object sender, EventArgs e)
@@ -35,45 +39,90 @@ namespace Dota2BuyMonitor
             timer1.Stop();
 
             //start the backgroundworkers
-            this.backgroundWorker1.RunWorkerAsync();
+            backgroundWorker1.RunWorkerAsync();
 
         }
 
         //declare needed vars
-        List<string> items = new List<string>();
+        List<string> items_old = new List<string>();
+        List<string> items_new = new List<string>();
+        List<string> items_final = new List<string>();
+        
+        bool firstTime = true;
         string itemname;
+
+
+        
 
         private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
         {
+            //items list so that it only contains the new items
+            items_new.Clear();
+
+            //init htmlagilitypack things
             HtmlWeb web = new HtmlWeb();
 
             new WebClient();
-            HtmlAgilityPack.HtmlDocument document = web.Load("http://www.dota2wh.com/dota/hero/Jugger");
+            HtmlAgilityPack.HtmlDocument document = web.Load("http://www.dota2wh.com/dota/hero/Jugger");//using a custom url so that it needs only to load minimal things
             try
             {
+                //get all recent items from WH webpage
                 foreach (HtmlNode node in document.DocumentNode.SelectNodes("//li[@class='recent-item']"))
                 {
-                    this.itemname = node.InnerText.Trim();
+                    this.itemname = node.InnerText.Trim();//get item name from html li node
 
-                    //exclude unwanted and already known items
-                    if (!items.Contains(itemname) && itemname != "Treasure Key")
+                    //exclude unwanted items
+                    if (itemname != "Treasure Key")
                     {
-                        items.Add(itemname);
-                        richTextBox1.Text += (itemname) + "\r\n";
+                        if (firstTime)
+                        {
+                            items_old.Add(itemname);
+                        }
+                        else
+                        {
+                            items_new.Add(itemname);
+                        }
                     }
-
                 }
             }
             catch
             {
-                richTextBox1.Text += "Error at Itemselection\r\n";
+                richTextBox1.Text += "Error getting/filtering itemlist\r\n";
             }
 
+
+            if (firstTime)
+            {
+                items_final = items_old;
+            }
+            else
+            {
+                //exclude already known items
+                //remove all old items from the final item list...only new items should be looked up
+                items_final = items_new.ToList();
+                items_final.Remove(items_old[0]);
+                items_final.Remove(items_old[1]);
+                items_final.Remove(items_old[2]);
+                items_final.Remove(items_old[3]);
+                items_final.Remove(items_old[4]);
+            }
         }
 
         private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
         {
+            richTextBox1.Clear();
 
+            foreach (string item_output in items_final)
+            {
+                richTextBox1.Text += item_output + "\r\n";
+            }
+
+            if (!firstTime)
+            {
+                items_old = items_new.ToList();
+            }
+            firstTime = false;
+            groupBoxOptions.Enabled = true;
         }
 
     }
